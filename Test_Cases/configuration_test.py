9dec8cba7962e4aacaefc678bd5db5adb6f4c814
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-11 10:45:03 +0530

    Data file and login page locators added.

---
+++ Test_Cases/configuration_test.py
@@ -0,0 +1,38 @@
+import allure
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+from webdriver_manager.firefox import GeckoDriverManager
+from Utilities.utils_config_reader import configuration_reader
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+
+@pytest.fixture()
+def log_on_failure(request, get_browser):
+    yield
+    item = request.node
+    driver = get_browser
+    if item.rep_call.failed:
+        allure.attach(driver.get_screenshot_as_png(), name="do_login", attachment_type=AttachmentType.PNG)
+
+
+@pytest.fixture(params=["chrome", "firefox"], scope="function")
+def get_browser(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    if request.param == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    request.cls.driver = driver
+    driver.get(configuration_reader("basic configuration", "test_url"))
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
