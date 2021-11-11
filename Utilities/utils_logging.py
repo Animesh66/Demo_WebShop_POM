34e9fc72e41860bf47c9311ef10dc293bdb2e0f9
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-11 16:42:01 +0530

    Data file and login page locators added.

--- Utilities/utils_logging.py
+++ Utilities/utils_logging.py
@@ -11,7 +11,7 @@ class Logger():
         fmt = logging.Formatter('%(asctime)s - %(filename)s:[%(lineno)s] - [%(levelname)s] - %(message)s')
 
         curr_time = time.strftime("%Y-%m-%d")
-        self.LogFileName = '..\\Logs\\log' + curr_time + '.txt'
+        self.LogFileName = '.\\Logs\\log' + curr_time + '.txt'
         # "a" to append the logs in same file, "w" to generate new logs and delete old one
         fh = logging.FileHandler(self.LogFileName, mode="a")
         fh.setFormatter(fmt)
