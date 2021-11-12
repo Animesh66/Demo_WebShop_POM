3615aac0b53dc8316215b724315dcc03f0aedfc5
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-12 14:10:00 +0530

    Verify product review test added.

--- Pages/Product_Review_Page.py
+++ Pages/Product_Review_Page.py
@@ -7,7 +7,7 @@ class ProductReviewPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def search_reviews(self, created_from_date, created_to_date):
+    def search_reviews_input(self, created_from_date, created_to_date):
         self.type("created_from_date_XPATH", created_from_date)
         self.type("created_to_date_XPATH", created_to_date)
         self.click("search_button_XPATH")
--- Test_Cases/test_verify_product_review.py
+++ Test_Cases/test_verify_product_review.py
@@ -1,4 +1,4 @@
-from Pages.Home_Page import HomePage
+from Pages.Login_Page import LoginPage
 from Test_Cases.Base_Test import BaseTest
 import pytest
 from Utilities.data_provider import get_data
@@ -6,7 +6,8 @@ from Utilities.data_provider import get_data
 
 class TestVerifyProductReview(BaseTest):
 
-    @pytest.mark.parametrize("created_from_date, created_to_date", get_data("sheet_name"))
-    def test_verify_product_review(self, created_from_date, created_to_date):
-        home = HomePage(self.driver)
-        home.navigate_product_review()
+    @pytest.mark.parametrize("email, password, created_from_date, created_to_date", get_data("sheet_name"))
+    def test_verify_product_review(self, email, password, created_from_date, created_to_date):
+        login = LoginPage(self.driver)
+        login.perform_login(email, password).navigate_product_review().search_reviews_input(created_from_date, created_to_date)
+
