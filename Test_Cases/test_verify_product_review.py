0ceff43ec3590c227aaddacf38c2e25b7f5e3aca
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-12 12:51:03 +0530

    Data file and login page locators added.

--- Test_Cases/test_verify_product_review.py
+++ Test_Cases/test_verify_product_review.py
@@ -3,7 +3,7 @@ import pytest
 from Utilities.data_provider import get_data
 
 
-class Test_VerifyProductReview(BaseTest):
+class TestVerifyProductReview(BaseTest):
 
     @pytest.mark.parametrize("created_from_date, created_to_date", get_data("sheet_name"))
     def test_verify_product_review(self, created_from_date, created_to_date):
