c962ca884bb4da1dc94756f006068c723ef40398
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-12 11:09:49 +0530

    Data file and login page locators added.

---
+++ Pages/Home_Page.py
@@ -0,0 +1,10 @@
+from Pages.Base_Page import BasePage
+
+
+class HomePage(BasePage):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def navigate_product_review(self):
+        pass
