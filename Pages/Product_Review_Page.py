afa6ff9ded8dd819232fff636c01c4bddcc54a5d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-12 12:20:52 +0530

    Data file and login page locators added.

--- Pages/Product_Review_Page.py
+++ Pages/Product_Review_Page.py
@@ -7,8 +7,8 @@ class ProductReviewPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def verify_created_date(self):
-        self.type("created_from_date_XPATH", "1/1/2017")
-        self.type("created_to_date_XPATH", "12/11/2021")
+    def search_reviews(self, created_from_date, created_to_date):
+        self.type("created_from_date_XPATH", created_from_date)
+        self.type("created_to_date_XPATH", created_to_date)
         self.click("search_button_XPATH")
 
