193b34c367d6fe3bb2ae8834d8d3557b54125d7d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-24 14:22:46 +0530

    Search product review test added.

--- Utilities/utils_logging.py
+++ Utilities/utils_logging.py
@@ -7,9 +7,7 @@ class Logger():
     def __init__(self, logger, file_level=logging.INFO):
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
-
         fmt = logging.Formatter('%(asctime)s - %(filename)s:[%(lineno)s] - [%(levelname)s] - %(message)s')
-
         curr_time = time.strftime("%Y-%m-%d")
         self.LogFileName = '.\\Logs\\log' + curr_time + '.txt'
         # "a" to append the logs in same file, "w" to generate new logs and delete old one
