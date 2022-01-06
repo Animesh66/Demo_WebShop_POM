9c89de36ad94808fb63a207aff3290a5930ab1b0
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 18:10:11 +0530

    Search product review test added.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='ui_pytest_ecom_example',
+      version='1.0',
+      description='Ecommerce project with collab f/w',
+      author='Animesh Mukherjee',
+      platforms='Web Application',
+      packages=find_packages()
+      )
